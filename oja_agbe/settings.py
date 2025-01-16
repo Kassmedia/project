aafbb21e40ba6f9086a3f0209f4033c73b95405e
oolayinka <wabsid01@gmail.com>
2025-01-16 13:16:47 +0100

    deploy

--- oja_agbe/settings.py
+++ oja_agbe/settings.py
@@ -12,7 +12,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'project-qv3p.onrender.com').split(',')
+ALLOWED_HOSTS = [ 'project-qv3p.onrender.com' ]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
